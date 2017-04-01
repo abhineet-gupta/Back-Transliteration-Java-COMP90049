8e1f7a2d612624f8cffa05e7330c90d8151fbb6e
Abhineet Gupta <a.gupta1905@gmail.com>
2017-04-01 22:09:07 +1100

    Implemented GED with custom scoring matrix

--- src/BackTransliteration.java
+++ src/BackTransliteration.java
@@ -10,17 +10,24 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class BackTransliteration {   
-    public static void main(String[] args) {
+        //Process entire file or limited records
+    private static final Boolean process_entire_file = false; //set to false to process limited records
+    private static final Integer proc_limit = 20;  //number of names to process; only valid if above is false
+    
+    private static final int max_row = 26, max_col = 26;    //number of letters in alphabet
+    private static int[][] matrix = new int[max_row][max_col];  //BLOSUM-style matrix to hold scoring matrix
+    
         //Path to files
-        String train_filepath = "D:\\GDrive\\Docs\\2017\\Code\\Java\\KT_Proj1_BackTransliteration\\data\\train.txt";
-        String dict_filepath = "D:\\GDrive\\Docs\\2017\\Code\\Java\\KT_Proj1_BackTransliteration\\data\\names.txt";
-        String DELIMITER = "\t";
-        
-        Boolean process_entire_file = false; //set to false to process limited records, as specified below
-        Integer proc_limit = 100;  //number of names to process; can be used to limit processing during testing
+    private static final String train_filepath = "D:\\GDrive\\Docs\\2017\\Code\\Java\\KT_Proj1_BackTransliteration\\data\\train.txt";
+    private static final String dict_filepath = "D:\\GDrive\\Docs\\2017\\Code\\Java\\KT_Proj1_BackTransliteration\\data\\names.txt";
+    private static final String DELIMITER = "\t";
+    
+    public static void main(String[] args) {
         
         int p_idx = 0;  //Persian name index
         int l_idx = 1;  //Latin name index
+        
+        int temp_int_score_value = 99;
         
         List<String[]> nameList = new ArrayList<>();    //to store data from train.txt
         List<String> dictList = new ArrayList<>();      //to store data from names.txt
@@ -62,15 +69,17 @@ public class BackTransliteration {
             num_names = proc_limit;
         }
         Map<String, ArrayList<String>> scoreNamesMap = new HashMap<>(); //to store the results/scores
-        int temp_score, temp_min = 99;
-        String temp_name;
+
+        int temp_score, temp_min;
+        String temp_name = "";
         
         for (int i = 0; i < num_names; i++) {   //for each Persian name
-            temp_min = 99;
+            temp_min = temp_int_score_value;
             temp_name = nameList.get(i)[p_idx].toLowerCase();
             
             for (int j = 0; j < dictList.size(); j++) {    //find its GED for each dictionary name
                 temp_score = calculateLevenshteinDistance(temp_name, dictList.get(j));
+//                temp_score = calculateGED(temp_name, dictList.get(j));
                 
                 //if the new GED is less than previous minimum GED for this Persian name...
                 if (temp_min > temp_score) {
@@ -101,7 +110,8 @@ public class BackTransliteration {
             }
             total_predicted += scoreNamesMap.get(nameList.get(i)[p_idx]).size();
         }
-        System.out.println("\nPrecision: " + correct_predicted*100/total_predicted + "%");
+        System.out.println("\nRecords processed: " + num_names);
+        System.out.println("Precision: " + correct_predicted*100/total_predicted + "%");
         System.out.println("Recall: " + correct_predicted*100/num_names + "%");
         System.out.println("Average # predictions/name: " + total_predicted/num_names);
         
@@ -150,10 +160,8 @@ public class BackTransliteration {
     private static int calculateGED(String lhs, String rhs) {
         int[][] distance = new int[lhs.length() + 1][rhs.length() + 1];        
         
-        int m_cost = 0;
         int i_cost = 1;
         int d_cost = 1;
-        int r_cost = 1;
         
         //Initialise first row and column of table 
         for (int i = 0; i <= lhs.length(); i++)                                 
@@ -173,11 +181,44 @@ public class BackTransliteration {
         return distance[lhs.length()][rhs.length()];
     }
     
+    /**
+     * @param c1: character 1 to be matched
+     * @param c2: character 2 to be matched with
+     * @return value of their match/replace score
+     */
     private static int matchReplaceCost(char c1, char c2) {
+        int c1_value = Character.getNumericValue(c1) - 10;
+        int c2_value = Character.getNumericValue(c2) - 10;
+        
+            //Default match and replacement scores
+        int def_match_score = 0;
+        int def_replace_score = 1;
+        
+            //Check if characters are outside A-Z; return 0 if so.
+        if (c1_value < 0 || c1_value > 25) {
+            if (c1 == c2) {
+                return def_match_score;   //if they are both the same non-[A-Z] characters, it's still a match 
+            } else {
+                return def_replace_score;   //
+            }
+        } else if (c2_value < 0 || c2_value > 25) {
+            return def_replace_score;
+        }
         
         
         
-        return 0;
+        //Initialise matrix
+        for (int row = 0; row < max_row; row++) {
+            for (int col = 0; col < max_col; col++) {
+                if (row == col) {
+                    matrix[row][col] = def_match_score;
+                } else {
+                    matrix[row][col] = def_replace_score;
+                }
+            }
+        }
+        
+        return matrix[c1_value][c2_value];
     }
 
     private static int minimum(int a, int b, int c) {                            
